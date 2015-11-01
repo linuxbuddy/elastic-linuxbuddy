6f0b7f2b6f7cb7b6397691de8cdd514e68f56213
Eric Mugerwa <ericmdev@gmail.com>
2015-11-01 20:40:52 +0000

    Install Apache

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -12,3 +12,21 @@ sudo yum install vim -y
 echo -e "\n--- Installing Git ---\n"
 sudo yum install git -y
 
+echo -e "\n--- Installing Apache ---\n"
+sudo yum install httpd -y
+
+echo -e "\n--- Configuring Autostart Apache ---\n"
+sudo chkconfig httpd on
+
+echo -e "\n--- Setting link /vagrant/public /var/www ---\n"
+if ! [ -L /var/www ]; then
+  sudo rm -rf /var/www
+  sudo ln -fs /vagrant/ /var/www
+fi
+
+echo -e "\n--- Adding httpd.conf ---\n"
+sudo cp /vagrant/provisioning/files/etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf
+
+echo -e "\n--- Adding 00-linuxbuddy.dev.conf ---\n"
+sudo cp /vagrant/provisioning/files/etc/httpd/conf.d/00-linuxbuddy.dev.conf /etc/httpd/conf.d/00-linuxbuddy.dev.conf
+
